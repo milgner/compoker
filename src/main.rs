e4dc78d6530ba50c8f1df25bf1bb0ff466b5a023
Marcus Ilgner <mail@marcusilgner.com>
2021-09-12 03:21:24 +0200

    feat: join & disconnect logic

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::time::{Duration, Instant};
-use std::collections::HashMap;
+
 use actix::prelude::*;
 use actix_files::{Files, NamedFile};
 use actix_web::{App, Error, HttpRequest, HttpResponse, HttpServer, web};
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web_actors::ws;
-use serde::{ Serialize, Deserialize };
+
+use crate::poker_server::*;
 
 mod poker_server;
 
@@ -14,75 +15,24 @@ const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
 /// How long before lack of client response causes a timeout
 const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
-#[derive(Serialize, Deserialize, Debug)]
-enum Vote {
-    Unknown,
-    One,
-    Two,
-    Three,
-    Five,
-    Eight,
-    Thirteen,
-    TwentyOne,
-    Infinite
-}
-
-
-
-#[derive(Debug, Serialize, Deserialize)]
-enum VotingState {
-    Opening,
-    Voting,
-    Closing
-}
-
-struct VotingParticipant {
-    id: String,
-    name: String,
-    // TODO: add reference to session or so...
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-struct VotingIssue {
-    id: String,
-    state: VotingState,
-    trello_card: Option<String>, // further abstraction TBD
-    outcome: Option<Vote>,
-    // participant id to votes
-    votes: HashMap<String, Vote>,
-}
-
-struct VotingSession {
-    id: String,
-    participants: Vec<VotingParticipant>,
-    current_issue: VotingIssue,
-}
 
 struct ClientConnection {
     hb: std::time::Instant,
-    server: Addr<poker_server::Server>,
-    participant: Option<VotingParticipant>,
+    server: Addr<Server>,
+    participant_id: usize,
+    session_id: usize, // ensure that the client will only ever be in one session - keinen Quatsch machen!
 }
 
 impl ClientConnection {
-    pub fn new(server: Addr<poker_server::Server>) -> ClientConnection {
+    pub fn new(server: Addr<Server>) -> ClientConnection {
         ClientConnection {
             hb: Instant::now(),
-            participant: None,
+            participant_id: 0,
+            session_id: 0,
             server,
         }
     }
 }
-
-// fn serialize_message(message: SocketMessage) -> Option<String> {
-//     let serialized = serde_json::to_string(&message);
-//     if serialized.is_ok() {
-//         Option::Some(serialized.unwrap())
-//     } else {
-//         println!("Failed to serialize message {:?}", message);
-//         Option::None
-//     }
-// }
 
 impl Actor for ClientConnection {
     type Context = ws::WebsocketContext<Self>;
@@ -90,6 +40,28 @@ impl Actor for ClientConnection {
     /// Method is called on actor start. We start the heartbeat process here.
     fn started(&mut self, ctx: &mut Self::Context) {
         self.start_heartbeat(ctx);
+        let addr = ctx.address();
+        self.server.send(Connect { addr: addr.recipient() })
+            .into_actor(self)
+            .then(|res, act, ctx| {
+                match res {
+                    Ok(res) => act.participant_id = res,
+                    _ => ctx.stop(),
+                }
+                fut::ready(())
+            })
+            .wait(ctx);
+    }
+
+    fn stopping(&mut self, _: &mut Self::Context) -> Running {
+        if self.participant_id > 0 {
+            self.server.do_send(Disconnect {
+                participant_id: self.participant_id,
+                session_id: self.session_id });
+        } else {
+            println!("Something is fishy: stopping before participant_id was set");
+        }
+        Running::Stop
     }
 }
 
@@ -97,7 +69,7 @@ impl ClientConnection {
     /// helper method that sends ping to client every second.
     ///
     /// also this method checks heartbeats from client
-    fn start_heartbeat(&self, ctx: &mut <Self as Actor>::Context) {
+    fn start_heartbeat(&self, ctx: &mut ws::WebsocketContext<Self>) {
         ctx.run_interval(HEARTBEAT_INTERVAL, |act, ctx| {
             // check client heartbeats
             if Instant::now().duration_since(act.hb) > CLIENT_TIMEOUT {
@@ -105,6 +77,7 @@ impl ClientConnection {
                 println!("Websocket Client heartbeat failed, disconnecting!");
 
                 // stop actor
+                // TODO: check whether this triggers the stopping() method and sends Disconnect
                 ctx.stop();
 
                 // don't try to send a ping
@@ -115,8 +88,33 @@ impl ClientConnection {
         });
     }
 
-    fn process_message(&self, message: poker_server::PokerMessage) {
-        println!("deserialized message = {:?}", message);
+    // invoked when a message from the browser has been received
+    fn process_message(&self, message: PokerMessage) {
+        let message = match message {
+            PokerMessage::CreateSessionRequest { participant_name, .. } =>
+                PokerMessage::CreateSessionRequest { participant_id: self.participant_id, participant_name },
+            PokerMessage::VoteRequest { issue_id, vote, .. } =>
+                PokerMessage::VoteRequest { participant_id: self.participant_id, issue_id, vote },
+            PokerMessage::JoinSessionRequest { session_id, participant_name, .. } =>
+                PokerMessage::JoinSessionRequest { participant_id: self.participant_id, session_id, participant_name },
+            _ => message
+        };
+        self.server.do_send(message);
+    }
+}
+
+// invoked when the server sends back a message -> forward it through the socket
+impl Handler<PokerMessage> for ClientConnection {
+    type Result = ();
+
+    fn handle(&mut self, msg: PokerMessage, ctx: &mut Self::Context) {
+        match msg {
+            // if the server sends back a session id, jot it down so we can use it for Disconnect
+            PokerMessage::JoinSessionRequest { session_id, .. } => self.session_id = session_id,
+            _ => ()
+        }
+        let serialized = serde_json::to_string(&msg).unwrap_or("Shit!".to_string());
+        ctx.text(serialized);
     }
 }
 
@@ -154,18 +152,14 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for ClientConnection 
     }
 }
 
-struct SessionCoordinator {
-    sessions: HashMap<String, VotingSession>,
-}
-
-async fn websocket(req: HttpRequest, stream: web::Payload, srv: web::Data<Addr<poker_server::Server>>) -> Result<HttpResponse, Error> {
+async fn websocket(req: HttpRequest, stream: web::Payload, srv: web::Data<Addr<Server>>) -> Result<HttpResponse, Error> {
     let connection = ClientConnection::new(srv.get_ref().clone());
     ws::start(connection, &req, stream)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let poker_server = poker_server::Server::new().start();
+    let poker_server = Server::new().start();
 
     let http_server = HttpServer::new(move || {
         App::new()
--- src/poker_server.rs
+++ src/poker_server.rs
@@ -1,24 +1,19 @@
 //! The `PokerServer` is an actor that maintains a list of sessions,
 //! their participants and current votes
 
-use actix::prelude::*;
-use rand::{self, Rng, thread_rng};
-use rand::distributions::Alphanumeric;
-use serde::{ Serialize, Deserialize };
+use std::collections::HashMap;
 
-use std::collections::{HashMap};
+use actix::prelude::*;
+use rand::{self, thread_rng, Rng};
+use serde::{Deserialize, Serialize};
 
 // helper function to generate a random id string
-fn generate_random_id() -> String {
-    thread_rng()
-        .sample_iter(&Alphanumeric)
-        .take(30)
-        .map(char::from)
-        .collect()
+fn generate_random_id() -> usize {
+    thread_rng().gen::<usize>()
 }
 
 #[derive(Message)]
-#[rtype(result = "String")] // return session id
+#[rtype(result = "usize")] // return participant id
 pub struct Connect {
     pub addr: Recipient<PokerMessage>,
 }
@@ -26,47 +21,63 @@ pub struct Connect {
 #[derive(Message)]
 #[rtype(result = "()")]
 pub struct Disconnect {
-    pub id: String,
+    pub participant_id: usize,
+    pub session_id: usize,
+}
+
+fn zero_usize() -> usize {
+    0
 }
 
 #[derive(Serialize, Deserialize, Debug, Message)]
 #[rtype(result = "()")] // responses are sent out asynchronously
+                        // participant ids always use Option<> so that they can be deserialized from JSON
+                        // the participant id is then filled in through the `ClientConnection`
 pub enum PokerMessage {
     CreateSessionRequest {
-        participant_name: String
+        #[serde(default = "zero_usize")]
+        participant_id: usize,
+        participant_name: String,
     },
     JoinSessionRequest {
-        session_id: String,
-        participant_name: String
+        #[serde(default = "zero_usize")]
+        participant_id: usize,
+        session_id: usize,
+        participant_name: String,
     },
     SessionInfoResponse {
-        session_id: String,
+        session_id: usize,
         current_issue: VotingIssue,
-        current_participants: Vec<String>
+        current_participants: Vec<String>,
+    },
+    SessionUnknownResponse {
+        session_id: usize,
     },
     ParticipantJoinAnnouncement {
-        participants: Vec<String>
+        participant_name: String,
     },
     ParticipantLeaveAnnouncement {
-        participants: Vec<String>
+        participant_name: String,
     },
     VotingIssueAnnouncement {
-        issue: VotingIssue
+        issue: VotingIssue,
     },
     VoteRequest {
-        issue_id: String,
-        vote: Vote
+        #[serde(default = "zero_usize")]
+        participant_id: usize,
+        issue_id: usize,
+        vote: Vote,
     },
     VoteReceipt,
     VotingResultsRevelation {
-        issue_id: String,
+        issue_id: usize,
         votes: HashMap<String, Vote>,
-        outcome: Vote
+        outcome: Vote,
     },
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
-enum Vote {
+pub enum Vote {
     Unknown,
     One,
     Two,
@@ -75,18 +86,18 @@ enum Vote {
     Eight,
     Thirteen,
     TwentyOne,
-    Infinite
+    Infinite,
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
-enum VotingState {
+pub enum VotingState {
     Opening,
     Voting,
-    Closing
+    Closing,
 }
 
-struct VotingParticipant {
-    id: String,
+pub struct VotingParticipant {
+    id: usize,
     name: String,
 }
 
@@ -100,19 +111,17 @@ impl Clone for VotingParticipant {
 }
 
 impl VotingParticipant {
-    pub fn new(name: String) -> VotingParticipant {
-        VotingParticipant {
-            id: generate_random_id(),
-            name
-        }
+    pub fn new(id: usize, name: String) -> VotingParticipant {
+        VotingParticipant { id, name }
     }
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct VotingIssue {
-    id: String,
+pub struct VotingIssue {
+    id: usize,
     state: VotingState,
-    trello_card: Option<String>, // further abstraction TBD
+    trello_card: Option<String>,
+    // further abstraction TBD
     outcome: Option<Vote>,
     // participant id to votes
     votes: HashMap<String, Vote>,
@@ -132,35 +141,49 @@ impl Clone for VotingIssue {
 
 impl VotingIssue {
     pub fn new() -> VotingIssue {
-        VotingIssue  {
+        VotingIssue {
             id: generate_random_id(),
             votes: HashMap::new(),
             outcome: None,
             state: VotingState::Opening,
-            trello_card: None
+            trello_card: None,
         }
     }
 }
 
 struct VotingSession {
-    id: String,
+    id: usize,
     participants: Vec<VotingParticipant>,
     current_issue: VotingIssue,
 }
 
 impl VotingSession {
-    pub fn new(session_id: String, initiator_name: String) -> VotingSession {
-        VotingSession  {
+    pub fn new(session_id: usize, initiator_id: usize, initiator_name: String) -> VotingSession {
+        VotingSession {
             id: session_id,
-            participants: vec!(VotingParticipant::new(initiator_name)),
-            current_issue: VotingIssue::new()
+            participants: vec![VotingParticipant::new(initiator_id, initiator_name)],
+            current_issue: VotingIssue::new(),
+        }
+    }
+
+    pub fn participant_names(&self) -> Vec<String> {
+        self.participants.iter().map(|p| p.name.clone()).collect()
+    }
+}
+
+impl Clone for VotingSession {
+    fn clone(&self) -> Self {
+        VotingSession {
+            id: self.id,
+            current_issue: self.current_issue.clone(),
+            participants: self.participants.clone(),
         }
     }
 }
 
 pub struct Server {
-    sessions: HashMap<String, VotingSession>,
-    clients: HashMap<String, Recipient<PokerMessage>>,
+    sessions: HashMap<usize, VotingSession>,
+    clients: HashMap<usize, Recipient<PokerMessage>>,
 }
 
 impl Server {
@@ -171,11 +194,27 @@ impl Server {
         }
     }
 
-    fn create_session(&mut self, initiator_name: String) -> String {
+    fn create_session(&mut self, initiator_id: usize, initiator_name: String) -> VotingSession {
         let session_id = generate_random_id();
-        let session = VotingSession::new(session_id.clone(), initiator_name);
-        self.sessions.insert(session_id.clone(), session);
-        session_id
+        let session = VotingSession::new(session_id, initiator_id, initiator_name);
+        self.sessions.insert(session_id, session.clone());
+        session
+    }
+
+    // dispatch the message to the right participant
+    fn send_message(&self, participant_id: usize, message: PokerMessage) {
+        if let Some((_, recipient)) = self
+            .clients
+            .iter()
+            .find(|entry| -> bool { *entry.0 == participant_id })
+        {
+            let _ = recipient.do_send(message);
+        } else {
+            println!(
+                "Trying to dispatch message to unknown participant {}",
+                participant_id
+            );
+        };
     }
 }
 
@@ -184,9 +223,9 @@ impl Actor for Server {
 }
 
 impl Handler<Connect> for Server {
-    type Result = String;
+    type Result = usize;
 
-    fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> String {
+    fn handle(&mut self, msg: Connect, _: &mut Context<Self>) -> usize {
         let client_id = generate_random_id();
         self.clients.insert(client_id.clone(), msg.addr);
         client_id
@@ -197,30 +236,107 @@ impl Handler<Disconnect> for Server {
     type Result = ();
 
     fn handle(&mut self, msg: Disconnect, _: &mut Self::Context) {
-        self.clients.remove(&msg.id);
+        if let Some(session) = self.sessions.get_mut(&msg.session_id) {
+            if session.participants.len() == 1 {
+                self.sessions.remove(&msg.session_id);
+            } else {
+                if let Some(pos) = session
+                    .participants
+                    .iter()
+                    .position(|p| p.id == msg.participant_id)
+                {
+                    let removed = session.participants.remove(pos);
+                    let participant_ids: Vec<usize> =
+                        session.participants.iter().map(|p| p.id).collect();
+                    participant_ids.iter().for_each(|p| {
+                        let message = PokerMessage::ParticipantLeaveAnnouncement {
+                            participant_name: removed.name.clone(),
+                        };
+                        self.send_message(*p, message);
+                    });
+                } else {
+                    println!("For some reason the participant wasn't in the expected session?!");
+                }
+            }
+        } else {
+            println!(
+                "Client is trying to leave non-existing session {}",
+                msg.session_id
+            );
+        }
+
+        self.clients.remove(&msg.participant_id);
     }
 }
 
 impl Handler<PokerMessage> for Server {
     type Result = ();
 
-    fn handle(&mut self, msg: PokerMessage, ctx: &mut Context<Self>) {
+    fn handle(&mut self, msg: PokerMessage, _: &mut Context<Self>) {
         match msg {
-            PokerMessage::CreateSessionRequest { participant_name } => {
-                let session_id = self.create_session(participant_name);
-                let session = self.sessions.get(session_id.as_str()).unwrap();
-                let current_participants = session.participants.iter().map( |participant| -> String {
-                    participant.name.clone()
-                }).collect();
-                ctx.notify(PokerMessage::SessionInfoResponse {
-                    session_id: session.id.clone(),
-                    current_issue: session.current_issue.clone(),
-                    current_participants
-                })
+            PokerMessage::CreateSessionRequest {
+                participant_id,
+                participant_name,
+            } => {
+                self.handle_create_session_request(participant_id, participant_name);
             }
+            PokerMessage::JoinSessionRequest {
+                participant_id,
+                participant_name,
+                session_id,
+            } => self.handle_join_session_request(session_id, participant_id, participant_name),
             _ => {
-                println!("Message not handled: {:?}", msg)
+                println!("Message not handled: {:?}", msg);
             }
+        }
+    }
+}
+
+impl Server {
+    fn handle_create_session_request(&mut self, participant_id: usize, participant_name: String) {
+        let session = self.create_session(participant_id, participant_name);
+        let current_participant_names = session.participant_names();
+        self.send_message(
+            participant_id,
+            PokerMessage::SessionInfoResponse {
+                session_id: session.id,
+                current_issue: session.current_issue,
+                current_participants: current_participant_names,
+            },
+        );
+    }
+
+    fn handle_join_session_request(
+        &mut self,
+        session_id: usize,
+        participant_id: usize,
+        participant_name: String,
+    ) {
+        if let Some(session) = self.sessions.get_mut(&session_id) {
+            let current_participant_ids: Vec<usize> =
+                session.participants.iter().map(|p| p.id).collect();
+            let message = PokerMessage::SessionInfoResponse {
+                session_id: session.id,
+                current_issue: session.current_issue.clone(),
+                current_participants: session.participant_names(),
+            };
+            // TODO: investigate parallelism
+            session.participants.push(VotingParticipant::new(
+                participant_id,
+                participant_name.clone(),
+            ));
+            self.send_message(participant_id, message);
+            current_participant_ids.iter().for_each(|participant_id| {
+                let message = PokerMessage::ParticipantJoinAnnouncement {
+                    participant_name: participant_name.clone(),
+                };
+                self.send_message(*participant_id, message);
+            });
+        } else {
+            self.send_message(
+                participant_id,
+                PokerMessage::SessionUnknownResponse { session_id },
+            );
         }
     }
 }
